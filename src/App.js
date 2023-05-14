fba5e9211d694f9f43cc8749438994400b205434
Maha-Engg <mahacm98@gmail.com>
2023-05-14 15:02:00 +0530

    add counter master

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React,{useState} from 'react';
+import './App.css';
+import   './App.css';
+const App=()=>{
+   const[state,setState]=useState('');
+   const handleChange=(event)=>{
+     setState(event.target.value);
+   }
+
+  return <>
+    
+    <div className='container'>
+
+        <h2> Real Time Counter   </h2>
+        <textarea className='text-area' onChange={handleChange} >       </textarea>
+        <div> <p className='counter' >   Character :{state.length} </p> </div>
+        
+    </div>
+  
+   </>
+
+
+}
+
+export default App;
